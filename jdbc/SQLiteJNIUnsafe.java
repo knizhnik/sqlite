e32bd0e59fb9e62fa395ed24fa37ec4ec72207f9
knizhnik <knizhnik@garret.ru>
2016-03-25 23:57:05 +0300

    Add SQLiteJNIUnsafe

---
+++ jdbc/SQLiteJNIUnsafe.java
@@ -0,0 +1,43 @@
+import sun.misc.Unsafe;
+import java.lang.reflect.*;
+
+public class SQLiteJNIUnsafe { 
+	public static native long select(int i);
+
+	static final String getFixedString(Unsafe unsafe, long addr, int size) { 
+		byte[] body = new byte[size];
+		unsafe.copyMemory(null, addr, body, 0, size);		
+		return new String(body);
+	}
+
+	public static void main(String[] args) throws Exception { 
+		Unsafe unsafe;
+		Constructor cc = Unsafe.class.getDeclaredConstructor();
+		cc.setAccessible(true);
+		unsafe = (Unsafe)cc.newInstance();	    
+		
+		System.loadLibrary("sqlite-jni-unsafe");
+		int nIterations = args.length > 0 ? Integer.parseInt(args[0]) : 1000000;
+		int nAccounts = args.length > 1 ? Integer.parseInt(args[1]) : 1500000;
+		long start = System.currentTimeMillis();
+		int n = 0;
+		for (int i = 0; i < nIterations; i++) { 
+			int orderkey = (int)(Math.random()*nAccounts);
+			long o = select(orderkey);
+			if (o != 0) {
+				Orders orders = new Orders(
+					unsafe.getInt(o+0), 
+					unsafe.getInt(o+4),
+ 				    unsafe.getByte(o+8),
+					unsafe.getDouble(o+16),
+					getFixedString(unsafe, o+24, 10),
+					getFixedString(unsafe, o+34, 15),
+					getFixedString(unsafe, o+49, 15),
+					unsafe.getInt(o+64),
+					getFixedString(unsafe, o+68, 79));
+				n += 1;
+			}
+		}
+		System.out.println("Elapsed time for selecting " + n + " objects: " + (System.currentTimeMillis() - start));
+	}
+}
---
+++ jdbc/sqlite-jni-unsafe.cpp
@@ -0,0 +1,57 @@
+#define _JNI_IMPLEMENTATION_ 1
+#include <jni.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <time.h>
+#include <sqlite3.h>
+
+sqlite3 *conn;
+sqlite3_stmt* stmt;
+
+#define CHECK(op) do { int error_ = (op); if (error_ != SQLITE_OK) { fprintf(stderr, "%s:%d: %s failed with code %d\n", __FILE__, __LINE__, #op, error_); exit(1); } } while (0)
+
+struct Orders { 
+    int o_orderkey;
+    int o_custkey;
+    char o_orderstatus;
+    double o_totalprice;
+    char o_orderdate[10];
+    char o_orderpriority[15];
+    char o_clerk[15];													
+    int o_shippriority;													
+    char o_comment[79];
+};
+	
+
+
+extern "C" {
+
+JNIEXPORT jlong Java_SQLiteJNIUnsafe_select(JNIEnv *env, jobject self, jint value)
+{
+	jlong result = 0;
+	static Orders o;
+	if (!stmt) {  
+		CHECK(sqlite3_open("test.db", &conn));
+		CHECK(sqlite3_prepare_v2(conn, "select * from orders where o_orderkey=?", -1, &stmt, NULL));
+	}
+	CHECK(sqlite3_bind_int(stmt, 1, value));
+	if (sqlite3_step(stmt) == SQLITE_ROW) {
+		o.o_orderkey = sqlite3_column_int(stmt, 0);
+		o.o_custkey = sqlite3_column_int(stmt, 1);
+		o.o_orderstatus  = (char)sqlite3_column_int(stmt, 2);		
+		o.o_totalprice = sqlite3_column_double(stmt, 3);
+		strncpy(o.o_orderdate, (char const*)sqlite3_column_text(stmt, 4), 10);
+		strncpy(o.o_orderpriority, (char const*)sqlite3_column_text(stmt, 5), 15);
+		strncpy(o.o_clerk, (char const*)sqlite3_column_text(stmt, 6), 15);
+		o.o_shippriority = sqlite3_column_int(stmt, 7);
+		strncpy(o.o_comment, (char const*)sqlite3_column_text(stmt, 8), 79);
+		result = (jlong)(size_t)&o;
+	}
+ 	CHECK(sqlite3_reset(stmt));	
+	return result;
+}
+
+}
